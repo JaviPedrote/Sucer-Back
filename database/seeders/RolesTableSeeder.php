560ae9e60b6bde3a29472c27d62507010d629edf
JaviPedrote <pedrote3.1416@gmail.com>
2025-05-12 11:13:01 +0200

    Seeder roles idempotente con updateOrCreate

--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -3,28 +3,24 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use App\Models\Role;
 
 class RolesTableSeeder extends Seeder
 {
     public function run(): void
     {
-        // 1) Desactiva temporalmente las FK para poder truncar
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        DB::table('roles')->truncate();
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
-
-        // 2) Define los roles
         $roles = [
-            ['id'   => Role::ADMIN, 'name' => 'Administrador', 'slug' => 'admin'],
-            ['id'   => Role::TUTOR, 'name' => 'Tutor',         'slug' => 'tutor'],
-            ['id'   => Role::USER,  'name' => 'Usuario',       'slug' => 'user'],
+            ['id' => Role::ADMIN, 'name' => 'Administrador', 'slug' => 'admin'],
+            ['id' => Role::TUTOR, 'name' => 'Tutor',         'slug' => 'tutor'],
+            ['id' => Role::USER,  'name' => 'Usuario',       'slug' => 'user'],
         ];
 
-        // 3) Insértalos
         foreach ($roles as $role) {
-            Role::create($role);
+            Role::updateOrCreate(
+                ['slug' => $role['slug']],      // clave única
+                ['id'   => $role['id'],         // valores a insertar/actualizar
+                 'name' => $role['name']]
+            );
         }
     }
 }
